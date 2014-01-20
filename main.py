c05c2b2429b7b8597989e89e1b32483f38e0dc2c
steven-mercatante <arrrms@gmail.com>
2014-01-20 12:39:44 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import json
+# --
+import conf
+# --
+from flask import Flask
+from flask import jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/sunrise/<int:zip_code>', methods=['GET'])
+def sunrise(zip_code):
+	if len(str(zip_code)) != 5:
+		return jsonify({'error': 'Invalid zip code: {0}'.format(zip_code )})
+	endpoint = 'http://api.wunderground.com/api/{0}/astronomy/q/{1}.json'.format(conf.API_KEY, zip_code)
+	r = requests.get(endpoint)
+	json_content = json.loads(r.content)
+
+	# Check if we got back valid data
+	if 'error' in json_content['response']:
+		return jsonify({'error': json_content['response']['error']['description']})
+
+	resp = jsonify({
+		'sunrise': '{0}:{1}'.format(
+			json_content['sun_phase']['sunrise']['hour'],
+			json_content['sun_phase']['sunrise']['minute']
+		)
+	})
+	return resp
+
+@app.route('/conditions/<int:zip_code>', methods=['GET'])
+def conditions(zip_code):
+	if len(str(zip_code)) != 5:
+		return jsonify({'error': 'Invalid zip code: {0}'.format(zip_code )})
+	endpoint = 'http://api.wunderground.com/api/{0}/conditions/q/CA/{1}.json'.format(conf.API_KEY, zip_code)
+	r = requests.get(endpoint)
+	json_content = json.loads(r.content)
+
+	# Check if we got back valid data
+	if 'error' in json_content['response']:
+		return jsonify({'error': json_content['response']['error']['description']})
+
+	resp = jsonify({
+		'condition': json_content['current_observation']['weather']
+	})
+	return resp
+
+if __name__ == '__main__':
+	app.run(debug=True)
