c36e6e0e0e377ed28b07e1a3b00563c3ec8567ba
steven-mercatante <arrrms@gmail.com>
2014-01-20 13:49:54 -0500

    test ENV var

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 import json
 # --
-import conf
+# import conf
 import errors
 # --
 from flask import Flask
@@ -11,44 +12,44 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-	return 'Hello'
+	return 'Hello', os.environ['API_KEY']
 
-@app.route('/sunrise/<int:zip_code>', methods=['GET'])
-def sunrise(zip_code):
-	if len(str(zip_code)) != 5:
-		return jsonify({'error': errors.INVALID_ZIP_CODE.format(zip_code )})
-	endpoint = 'http://api.wunderground.com/api/{0}/astronomy/q/{1}.json'.format(conf.API_KEY, zip_code)
-	r = requests.get(endpoint)
-	json_content = json.loads(r.content)
+# @app.route('/sunrise/<int:zip_code>', methods=['GET'])
+# def sunrise(zip_code):
+# 	if len(str(zip_code)) != 5:
+# 		return jsonify({'error': errors.INVALID_ZIP_CODE.format(zip_code )})
+# 	endpoint = 'http://api.wunderground.com/api/{0}/astronomy/q/{1}.json'.format(conf.API_KEY, zip_code)
+# 	r = requests.get(endpoint)
+# 	json_content = json.loads(r.content)
 
-	# Check if we got back valid data
-	if 'error' in json_content['response']:
-		return jsonify({'error': json_content['response']['error']['description']})
+# 	# Check if we got back valid data
+# 	if 'error' in json_content['response']:
+# 		return jsonify({'error': json_content['response']['error']['description']})
 
-	resp = jsonify({
-		'sunrise': '{0}:{1}'.format(
-			json_content['sun_phase']['sunrise']['hour'],
-			json_content['sun_phase']['sunrise']['minute']
-		)
-	})
-	return resp
+# 	resp = jsonify({
+# 		'sunrise': '{0}:{1}'.format(
+# 			json_content['sun_phase']['sunrise']['hour'],
+# 			json_content['sun_phase']['sunrise']['minute']
+# 		)
+# 	})
+# 	return resp
 
-@app.route('/conditions/<int:zip_code>', methods=['GET'])
-def conditions(zip_code):
-	if len(str(zip_code)) != 5:
-		return jsonify({'error': errors.INVALID_ZIP_CODE.format(zip_code )})
-	endpoint = 'http://api.wunderground.com/api/{0}/conditions/q/CA/{1}.json'.format(conf.API_KEY, zip_code)
-	r = requests.get(endpoint)
-	json_content = json.loads(r.content)
+# @app.route('/conditions/<int:zip_code>', methods=['GET'])
+# def conditions(zip_code):
+# 	if len(str(zip_code)) != 5:
+# 		return jsonify({'error': errors.INVALID_ZIP_CODE.format(zip_code )})
+# 	endpoint = 'http://api.wunderground.com/api/{0}/conditions/q/CA/{1}.json'.format(conf.API_KEY, zip_code)
+# 	r = requests.get(endpoint)
+# 	json_content = json.loads(r.content)
 
-	# Check if we got back valid data
-	if 'error' in json_content['response']:
-		return jsonify({'error': json_content['response']['error']['description']})
+# 	# Check if we got back valid data
+# 	if 'error' in json_content['response']:
+# 		return jsonify({'error': json_content['response']['error']['description']})
 
-	resp = jsonify({
-		'condition': json_content['current_observation']['weather']
-	})
-	return resp
+# 	resp = jsonify({
+# 		'condition': json_content['current_observation']['weather']
+# 	})
+# 	return resp
 
 if __name__ == '__main__':
 	app.run()
